cd90c8e38c7724f7b7116797466a7187ccc09493
hari185pr <haribe185@gmail.com>
2025-07-14 16:34:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+
 import os
-import threading
-from flask import Flask
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from telegram import Update, Bot
-from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def keep_alive():
+def home():
     return "Bot is running!"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-print(f"Retrived BOT_TOKEN:{bool(BOT_TOKEN)}")
 bot = Bot(token=BOT_TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -21,13 +20,12 @@ async def roll(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🎲 You rolled a random waifu!")
 
 def run_bot():
-    print("starting bot..")
-    appbuilder = ApplicationBuilder().token(BOT_TOKEN).build()
-    appbuilder.add_handler(CommandHandler("start", start))
-    appbuilder.add_handler(CommandHandler("roll", roll))
-    appbuilder.run_polling()
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("roll", roll))
+    application.run_polling()
 
 if __name__ == "__main__":
-    print("launching server and bot")
+    import threading
     threading.Thread(target=lambda: app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))).start()
     run_bot()
